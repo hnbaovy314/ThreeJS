2014bfd796ec10159237eac950c5efe0289df9da
VyHuynh <hnbaovy314@gmail.com>
2018-05-16 16:34:30 +0700

    Creating folder tree, starting server

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/', (req, res) => {
+   res.sendFile('index.html', {
+     root: 'views'
+   });
+});
+
+
+
+module.exports = router;
