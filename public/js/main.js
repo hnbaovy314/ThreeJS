d1e4d8076ae924e2d545e2effbfebfb186a27503
Ngo Lap Nguyen <ngolap.nguyen@gmail.com>
2018-05-20 07:32:56 +0700

    Add element model page, switch (most of) the geometry to buffer geometry

--- public/js/main.js
+++ public/js/main.js
@@ -8,13 +8,14 @@ var demoData = [
     [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-var container, stats;
-var camera, scene, raycaster, renderer, controls;
+var container, stats, raycaster;
+var currentCamera, defaultCamera, currentScene, currentRenderer, currentControls;
+var tableScene, tableRenderer, tableControls;
 var mouse = new THREE.Vector2(), previewMouse = new THREE.Vector2(),
     INTERSECTED;
 
 // Variable
-var periodicTable;
+var periodicTable = [];
 var elementBoxSize = 3;
 var gridSeparation = 3.5;
 var elementTextGridSize = 0.8;
@@ -24,9 +25,9 @@ var firstSize = 0.8, secondSize = 0.3, thirdSize = 0.25, fourthSize = 0.15;
 // Build preview panel for element data preview
 var previewPanelSize = 200;
 var previewPanel = document.createElement('div');
-previewPanel.setAttribute("id", "preview-panel");
-previewPanel.style.width = previewPanelSize + "px";
-previewPanel.style.height = previewPanelSize + "px";
+previewPanel.setAttribute('id', 'preview-panel');
+previewPanel.style.width = previewPanelSize + 'px';
+previewPanel.style.height = previewPanelSize + 'px';
 previewPanel.style.opacity = 0;
 previewPanel.innerHTML = '' +
     '<h2>8 - Oxygen</h2>' +
@@ -36,6 +37,32 @@ previewPanel.innerHTML = '' +
     '</ul>' +
     '<h1 class="symbol">O</h1>';
 
+// Build data screen
+var elementCamera, elementScene, elementRenderer, elementControls; // Camera, scene, renderer & controls for element model
+var dataScreen = document.createElement('div');
+dataScreen.setAttribute('id', 'data-screen');
+// IMPORTANT!
+// Thông tin nguyên tử thì thêm vào phần left/right column
+// Cứ dùng document.createElement để tạo 1 cái div/p mới rồi append child vào
+// Tham khảo cách t build ở trên (cái previewPanel ấy)
+// Có gì thì hỏi
+// T về ngủ
+// Add left and right column
+newElement = document.createElement('div');
+newElement.setAttribute('id', 'ds-left-column');
+dataScreen.appendChild(newElement);
+newElement = document.createElement('div');
+newElement.setAttribute('id', 'ds-right-column');
+dataScreen.appendChild(newElement);
+// Add close button
+var newElement = document.createElement('div');
+newElement.setAttribute('id', 'ds-close-button');
+newElement.appendChild(document.createElement('b'));
+newElement.appendChild(document.createElement('b'));
+newElement.appendChild(document.createElement('b'));
+newElement.appendChild(document.createElement('b'));
+dataScreen.appendChild(newElement);
+
 init();
 animate();
 
@@ -43,56 +70,99 @@ function init() {
     container = document.createElement('div');
     document.body.appendChild(container);
 
-    camera = new THREE.PerspectiveCamera(
+    // Init for default camera, used throughout the app
+    defaultCamera = new THREE.PerspectiveCamera(
         70,
         window.innerWidth / window.innerHeight,
         1,
         1000
     );
-    camera.position.z = 30;
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    defaultCamera.position.z = 35;
+    defaultCamera.lookAt(new THREE.Vector3(0, 0, 0));
+    currentCamera = defaultCamera;
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xf2f2f2);
-    
+    // Init for periodic table scene as the default scene
+    tableScene = new THREE.Scene();
+    tableScene.background = new THREE.Color(0xf2f2f2);
+    currentScene = tableScene;
+
+    tableRenderer = new THREE.WebGLRenderer();
+    tableRenderer.setPixelRatio(window.devicePixelRatio);
+    tableRenderer.setSize(window.innerWidth, window.innerHeight);
+    tableRenderer.setClearColor(0xF2F2F2, 1);
+    currentRenderer = tableRenderer;
+
+    container.appendChild(currentRenderer.domElement);
+    container.appendChild(previewPanel);
+    container.appendChild(dataScreen);
+
+    currentScene.add(getElementGrid(demoData));
+
     raycaster = new THREE.Raycaster();
+    currentRaycastTarget = periodicTable;
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xF2F2F2, 1);
-    container.appendChild(renderer.domElement);
+    // Init controls for periodic table scene
+    tableControls = new THREE.OrbitControls(defaultCamera, tableRenderer.domElement);
+    tableControls.enableDamping = true;
+    tableControls.dampingFactor = 0.1;
+    tableControls.rotateSpeed = 0.25;
+    tableControls.zoomSpeed = 5;
+    tableControls.minDistance = 10;
+    tableControls.maxDistance = 40;
+    tableControls.maxPolarAngle = Math.PI * 5 / 6;
+    tableControls.minPolarAngle = Math.PI / 6;
+    tableControls.maxAzimuthAngle = Math.PI / 4;
+    tableControls.minAzimuthAngle = -Math.PI / 4;
+    tableControls.enablePan = false;
+    currentControls = tableControls;
 
+    // Init for element detail data screen
+    // with different camera, scene, renderer and controls
+    elementCamera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    );
+    elementCamera.position.z = 50;
+    elementCamera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    elementScene = new THREE.Scene();
+
+    elementRenderer = new THREE.WebGLRenderer();
+    elementRenderer.setSize(window.innerWidth, window.innerHeight);
+    elementRenderer.setPixelRatio(window.devicePixelRatio);
+    elementRenderer.setClearColor(0xF2F2F2, 1);
+    var erContainer = document.createElement('div'); // Element Renderer Container
+    erContainer.setAttribute('id', 'ds-er-container');
+    erContainer.appendChild(elementRenderer.domElement);
+    dataScreen.appendChild(erContainer);
+
+    elementControls = new THREE.TrackballControls(elementCamera, elementRenderer.domElement);
+    elementControls.enableDamping = true;
+    elementControls.dampingFactor = 0.2;
+    elementControls.rotateSpeed = 2;
+    elementControls.zoomSpeed = 5;
+    elementControls.minDistance = 20;
+    elementControls.maxDistance = 80;
+    elementControls.noPan = true;
+
+    // Performance Stats
     stats = new Stats();
     container.appendChild(stats.dom);
 
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.1;
-    controls.rotateSpeed = 0.25;
-    controls.zoomSpeed = 5;
-    controls.minDistance = 10;
-    controls.maxDistance = 40;
-    controls.maxPolarAngle = Math.PI * 2 / 3;
-    controls.minPolarAngle = Math.PI / 3;
-    controls.maxAzimuthAngle = Math.PI / 4;
-    controls.minAzimuthAngle = -Math.PI / 4;
-    controls.enablePan = false;
-
-    container.appendChild(previewPanel);
+    // Add listeners
+    document.addEventListener('click', onTableBoxClick, false);
+    document.getElementById('ds-close-button').addEventListener('click', onElementModelCloseButtonClick, false);
 
     document.addEventListener('mousemove', onDocumentMouseMove, false);
-    document.addEventListener('click', onDocumentMouseClick, false);
     window.addEventListener('resize', onWindowResize, false);
-
-    periodicTable = getElementGrid(demoData);
-    scene.add(periodicTable);
 }
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    currentCamera.aspect = window.innerWidth / window.innerHeight;
+    currentCamera.updateProjectionMatrix();
+    currentRenderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function onDocumentMouseMove(event) {
@@ -106,16 +176,64 @@ function onDocumentMouseMove(event) {
 }
 
 // Listening for click event, combining with the INTERSECTED
-function onDocumentMouseClick(event) {
+// If click event happens when INTERSECTED is not null (meaning the user is clicking on an object),
+// get the data screen of the element
+function onTableBoxClick(event) {
     event.preventDefault();
 
     if (INTERSECTED) {
-        console.log("Ahihi");
+        // Return the raycast, INTERSECTED and preview panel to clean state
+        currentRaycastTarget = null;
+        INTERSECTED.children[0].material.opacity = 0.25;
+        INTERSECTED = null;
+        hidePreviewPanel();
+        document.body.style.cursor = "auto";
+
+        dataScreen.style.transform = 'translateY(-100vh)';
+        dataScreen.style.opacity = 1;
+
+        getElementModel();
+
+        // Switch to element model camera, scene & ...
+        elementCamera.position.z = 60;
+        currentCamera = elementCamera;
+        currentScene = elementScene;
+        currentRenderer = elementRenderer;
+        currentControls = elementControls;
     }
+}
+
+// Listening for click event on the element model panel close button
+function onElementModelCloseButtonClick(event) {
+    event.preventDefault();
+
+    dataScreen.style.transform = 'translateY(100vh)';
+    dataScreen.style.opacity = 0;
+
+    // Remove element model from elementScene
+    elementScene.remove(elementScene.getObjectByName('Element Model'));
+
+    // Switch to table camera, scene & ...
+    currentCamera = defaultCamera;
+    currentScene = tableScene;
+    currentRenderer = tableRenderer;
+    currentControls = tableControls;
+    currentRaycastTarget = periodicTable;
+
+    currentCamera.lookAt(new THREE.Vector3(0, 0, 0));
+    currentCamera.position.x = 0;
+    currentCamera.position.y = 0;
+    currentCamera.position.z = 20;
+    new TWEEN.Tween(currentCamera.position)
+        .to({z: 35}, 1000)
+        .easing(TWEEN.Easing.Exponential.Out)
+        .start();
 }
 
 // Get individual element
 function getElement(size) {
+    var group = new THREE.Group();
+
     // Create canvas and write text on it
     var canvas = document.createElement('canvas');
     var canvasSize = 256;
@@ -142,22 +260,34 @@ function getElement(size) {
     var texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
 
-    var geometry = new THREE.PlaneGeometry(size, size, size);
-    var material = new THREE.MeshBasicMaterial({
+    var planeGeometry = new THREE.PlaneBufferGeometry(size, size, size);
+    var planeMaterial = new THREE.MeshBasicMaterial({
         map: texture,
         side: THREE.DoubleSide,
         transparent: true
     });
-    var edges = new THREE.EdgesGeometry(geometry);
+
+    var boxGeometry = new THREE.BoxBufferGeometry(size, size, size / 2);
+    var boxMaterial = new THREE.MeshBasicMaterial({
+        opacity: 0,
+        transparent: true
+    });
+    var edges = new THREE.EdgesGeometry(boxGeometry);
     var line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: 0x1A1A1A}));
     line.material.depthTest = false;
     line.material.opacity = 0.25;
     line.material.transparent = true;
 
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.add(line);
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    var box = new THREE.Mesh(boxGeometry, boxMaterial);
+    box.add(line);
 
-    return mesh;
+    group.add(plane);
+    group.add(box);
+
+    periodicTable.push(box);
+
+    return group;
 }
 
 function getTableHeader(width, positionY) {
@@ -178,7 +308,7 @@ function getTableHeader(width, positionY) {
     var texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
 
-    var geometry = new THREE.PlaneGeometry(width, width / 8, 1);
+    var geometry = new THREE.PlaneBufferGeometry(width, width / 8, 1);
     var material = new THREE.MeshBasicMaterial({
         map: texture,
         side: THREE.DoubleSide,
@@ -195,7 +325,7 @@ function getTableHeader(width, positionY) {
 }
 
 function getTableBorder(width, height) {
-    var geometry = new THREE.PlaneGeometry(width * 1.25, height * 1.5, 1);
+    var geometry = new THREE.PlaneBufferGeometry(width * 1.25, height * 1.5, 1);
     var material = new THREE.MeshBasicMaterial({
         opacity: 0,
         transparent: true
@@ -221,7 +351,7 @@ function getElementGrid(table) {
             if (table[i][j]) {
                 var mesh = getElement(elementBoxSize);
                 mesh.position.x = j * gridSeparation - (gridSeparation * (table[0].length - 1)) / 2;
-                mesh.position.y = -(i * gridSeparation + mesh.geometry.parameters.height / 2) + (gridSeparation * (table.length - 1)) / 2;
+                mesh.position.y = -(i * gridSeparation + elementBoxSize / 2) + (gridSeparation * (table.length - 1)) / 2;
                 group.add(mesh);
             }
         }
@@ -229,8 +359,8 @@ function getElementGrid(table) {
 
     group.position.z += 1;
 
-    scene.add(getTableHeader(table[0].length * gridSeparation, gridSeparation * table.length));
-    scene.add(getTableBorder(table[0].length * gridSeparation, gridSeparation * table.length));
+    tableScene.add(getTableHeader(table[0].length * gridSeparation, gridSeparation * table.length));
+    tableScene.add(getTableBorder(table[0].length * gridSeparation, gridSeparation * table.length));
 
     return group;
 }
@@ -263,36 +393,177 @@ function hidePreviewPanel() {
     previewPanel.style.opacity = 0;
 }
 
+// Create orbit layer of eletrons
+function getElementModelOrbitLayer(electronNumber, radius, tilt) {
+    var orbitContainer = new THREE.Object3D();
+    orbitContainer.rotation.x = tilt;
+
+    var orbit = new THREE.Object3D();
+    var mergedGeometry = new THREE.Geometry();
+
+    var geometry = new THREE.CircleGeometry(radius, 100);
+    geometry.vertices.shift();
+    var line = new THREE.LineSegments(geometry, new THREE.LineBasicMaterial({color: 0x1A1A1A}));
+    line.material.depthTest = false;
+    line.material.transparent = true;
+
+    var electronGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+    var electronMaterial = new THREE.MeshBasicMaterial({
+        color: 0x1A1A1A,
+    });
+
+    var angle = 2 * Math.PI / electronNumber;
+    var count = 0;
+    for (var i = 0; i < electronNumber; i++) {
+        var x = Math.cos(angle * count) * radius;
+        var y = Math.sin(angle * count) * radius;
+        count++;
+
+        electronGeometry.translate(x, y, 0);
+
+        mergedGeometry.merge(electronGeometry);
+
+        electronGeometry.translate(-x, -y, 0);
+    }
+
+    orbit.add(line);
+    orbit.add(new THREE.Mesh(mergedGeometry, electronMaterial));
+
+    var tween = new TWEEN.Tween(orbit.rotation)
+        .to({z: '+' + Math.PI * 2}, 10000)
+        .repeat(Infinity)
+        .start();
+
+    orbitContainer.add(orbit);
+
+    return orbitContainer;
+}
+
+// Get element atomic model
+function getElementModel() {
+    var atomicNumber = 8; // = Number of protons = Number of electrons
+    var atomicWeight = 15.9994;
+    var neutronNumber = Math.round(atomicWeight) - atomicNumber;
+    
+    var group = new THREE.Group();
+    var mergedProtonGeometry = new THREE.Geometry(), mergedNeutronGeometry = new THREE.Geometry();
+    var geometry = new THREE.SphereGeometry(1, 32, 32);
+    var protonMaterial = new THREE.MeshPhongMaterial({
+        color: 0x4169E1
+    })
+    var neutronMaterial = new THREE.MeshPhongMaterial({
+        color: 0xDC143C
+    })
+
+    var radius = Math.pow(1.125, Math.floor(atomicNumber / 8));
+
+    // Create protons
+    for (var i = 0; i < atomicNumber; i++) {
+        var u = Math.random();
+        var v = Math.random();
+        var theta = 2 * Math.PI * u;
+        var phi = Math.acos(2 * v - 1);
+        var x = radius * Math.sin(phi) * Math.cos(theta);
+        var y = radius * Math.sin(phi) * Math.sin(theta);
+        var z = radius * Math.cos(phi);
+
+        geometry.translate(x, y, z);
+
+        mergedProtonGeometry.merge(geometry);
+
+        geometry.translate(-x, -y, -z);
+    }
+
+    // Create neutrons
+    for (var i = 0; i < neutronNumber; i++) {
+        var u = Math.random();
+        var v = Math.random();
+        var theta = 2 * Math.PI * u;
+        var phi = Math.acos(2 * v - 1);
+        var x = radius * Math.sin(phi) * Math.cos(theta);
+        var y = radius * Math.sin(phi) * Math.sin(theta);
+        var z = radius * Math.cos(phi);
+
+        geometry.translate(x, y, z);
+
+        mergedNeutronGeometry.merge(geometry);
+
+        geometry.translate(-x, -y, -z);
+    }
+
+    var outerGeometry = new THREE.SphereBufferGeometry(radius + 2.5, 32, 32);
+    var outerMaterial = new THREE.MeshBasicMaterial({
+        color: 0x1A1A1A,
+        opacity: 0.3,
+        transparent: true
+    })
+
+    group.add(new THREE.Mesh(mergedProtonGeometry, protonMaterial));
+    group.add(new THREE.Mesh(mergedNeutronGeometry, neutronMaterial));
+    group.add(new THREE.Mesh(outerGeometry, outerMaterial));
+    group.add(
+        // Get electron layer
+        // Cứ cho chạy loop, tính số electron ở từng lớp rồi gọi hàm
+        getElementModelOrbitLayer(8, radius + 7, 0)
+    );
+
+    group.name = "Element Model";
+    elementScene.add(group);
+
+    var spotLight1 = new THREE.SpotLight(0xF2F2F2);
+    spotLight1.position.set(0, 0, (radius + 5));
+    spotLight1.penumbra = 1;
+    spotLight1.lookAt = group;
+    spotLight1.castShadow = true;
+    spotLight1.intensity = 1;
+
+    var spotLight2 = new THREE.SpotLight(0xF2F2F2);
+    spotLight2.position.set(0, 0, -(radius + 5));
+    spotLight2.penumbra = 1;
+    spotLight2.lookAt = group;
+    spotLight2.castShadow = true;
+    spotLight2.intensity = 1;
+
+    elementScene.add(spotLight1);
+    elementScene.add(spotLight2);
+}
+
 function animate() {
     requestAnimationFrame(animate);
     render();
     stats.update();
-    controls.update();
+    currentControls.update();
+    TWEEN.update();
 }
 
-function render() {       
+function render() {
     // find intersections
-    raycaster.setFromCamera(mouse, camera);
-    var intersects = raycaster.intersectObjects(periodicTable.children);
-    if (intersects.length > 0) {
-        if (INTERSECTED != intersects[0].object) {
+    if (currentRaycastTarget) {
+        raycaster.setFromCamera(mouse, currentCamera);
+        var intersects = raycaster.intersectObjects(currentRaycastTarget);
+        if (intersects.length > 0) {
+            if (INTERSECTED != intersects[0].object) {
+                if (INTERSECTED) {
+                    INTERSECTED.children[0].material.opacity = 0.25;
+                    document.body.style.cursor = "auto";
+                    hidePreviewPanel();
+                }
+                
+                INTERSECTED = intersects[0].object;
+                INTERSECTED.children[0].material.opacity = 1;
+                document.body.style.cursor = "pointer";
+                getPreviewPanel();
+            }
+        } else {
             if (INTERSECTED) {
                 INTERSECTED.children[0].material.opacity = 0.25;
+                document.body.style.cursor = "auto";
                 hidePreviewPanel();
             }
-            
-            INTERSECTED = intersects[0].object;
-            INTERSECTED.children[0].material.opacity = 1;
-            getPreviewPanel();
-        }
-    } else {
-        if (INTERSECTED) {
-            INTERSECTED.children[0].material.opacity = 0.25;
-            hidePreviewPanel();
-        }
 
-        INTERSECTED = null;
+            INTERSECTED = null;
+        }
     }
-    
-    renderer.render(scene, camera);
+
+    currentRenderer.render(currentScene, currentCamera);
 }
